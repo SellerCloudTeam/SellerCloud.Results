a4460d06915de65c5ea1e18b5fd826b6d1795f08
Stanislav Chankov <stanislavchankov94@gmail.com>
2019-06-20 07:55:20 +0300

    Added Result{T} and ResultType

---
+++ src/SellerCloud.Results/ResultType.cs
@@ -0,0 +1,13 @@
+﻿namespace SellerCloud.Results
+{
+    public enum ResultType
+    {
+        NoError = 0,
+
+        NoData = 1,
+
+        ValidationError = 2,
+
+        PermissionError = 3,
+    }
+}
---
+++ src/SellerCloud.Results/Result{T}.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SellerCloud.Results
+{
+    public class Result<T>
+    {
+        public T Value { get; }
+
+        public bool HasValue => !EqualityComparer<T>.Default.Equals(Value, default(T));
+
+        public ResultType Type { get; set; }
+
+        public bool IsSuccess
+            => Type == ResultType.NoError
+            || Type == ResultType.NoData;
+
+        public Result(T value, ResultType resultType)
+        {
+            Value = value;
+            Type = resultType;
+        }
+
+        public static Task<Result<T>> Fail<T>(T value, ResultType resultType)
+        {
+            return Task.FromResult(new Result<T>(value, resultType));
+        }
+    }
+}
