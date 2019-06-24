4ee7ff7f4ad0161a5d32e8d067d792d808988677
Stanislav Chankov <chankov.s@sellercloud.com>
2019-06-24 16:26:00 +0300

    * Added non-generic AsResult extension method in ExceptionExtensions.

--- src/SellerCloud.Results/Extensions/ExceptionExtensions.cs
+++ src/SellerCloud.Results/Extensions/ExceptionExtensions.cs
@@ -2,11 +2,13 @@
 
 namespace SellerCloud.Results
 {
+    /// <summary>
+    /// Contains static extension methods for <see cref="System.Exception"/>, which wraps it in <see cref="Result{T}"/>/<see cref="Result"/>.
+    /// </summary>
     public static class ExceptionExtensions
     {
-        public static Result<T> AsResult<T>(this Exception exception)
-        {
-            return ResultFactory.Error<T>(exception);
-        }
+        public static Result<T> AsResult<T>(this Exception exception) => ResultFactory.Error<T>(exception);
+
+        public static Result AsResult(this Exception exception) => ResultFactory.Error(exception);
     }
 }
