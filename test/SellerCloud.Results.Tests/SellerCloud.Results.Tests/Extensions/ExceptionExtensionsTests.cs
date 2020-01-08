c9ca18ffc3ea2d20321975ba5a0ea77d6dbea517
Stanislav Chankov <chankov.s@sellercloud.com>
2020-01-08 15:27:35 +0200

    * Added Unit testsing project; * Added few unit tests;

---
+++ test/SellerCloud.Results.Tests/SellerCloud.Results.Tests/Extensions/ExceptionExtensionsTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SellerCloud.Results.Tests.Extensions
+{
+    [TestFixture]
+    public class ExceptionExtensionsTests
+    {
+        [Test]
+        public void AsResult_Exception_ReturnsUnsuccessFulResult()
+        {
+            // Arrange
+            var exception = new Exception();
+
+            // Act
+            var errorResult = exception.AsResult();
+
+            // Assert
+            Assert.AreEqual(false, errorResult.IsSuccessful);
+        }
+
+        [Test]
+        public void AsResult_Exception_ReturnsUnsuccessFulResultWithValue()
+        {
+            // Arrange
+            var exception = new Exception();
+
+            // Act
+            var errorResult = exception.AsResult<string>();
+
+            // Assert
+            Assert.AreEqual(false, errorResult.IsSuccessful);
+        }
+    }
+}
