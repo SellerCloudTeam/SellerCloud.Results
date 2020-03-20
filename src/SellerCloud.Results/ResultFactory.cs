21ea10ffaf37d7e2e6d99192eb8fd8af1fb738fa
zdravko-g <zdravko.g@sellercloud.com>
2020-03-20 12:53:56 +0200

    Added new Success method accepting string, required in SellerCloud.Net.Http

--- src/SellerCloud.Results/ResultFactory.cs
+++ src/SellerCloud.Results/ResultFactory.cs
@@ -11,6 +11,8 @@ namespace SellerCloud.Results
         public static Result Error(Exception exception) => new Result(errorMessage: exception.Message, errorSource: exception.StackTrace);
 
         public static Result<TData> Success<TData>(TData data) => new Result<TData>(data);
+        public static Result<TData> Success<TData>(string message) => new Result<TData>(message);
+
         public static Result<TData> Error<TData>(string message) => new Result<TData>(errorMessage: message);
         public static Result<TData> Error<TData>(string message, string? source) => new Result<TData>(errorMessage: message, errorSource: source);
         public static Result<TData> Error<TData>(Exception exception) => new Result<TData>(errorMessage: exception.Message, errorSource: exception.StackTrace);
