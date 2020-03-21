54d607ac0df2f1efb688c3dca7a4fce5a21db7ae
Stan <stanimir@sellercloud.com>
2020-03-21 20:05:15 +0200

    Adding HttpResult models

---
+++ src/SellerCloud.Results/Http/HttpResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace SellerCloud.Results.Http
+{
+    public class HttpResult : Result
+    {
+        internal HttpResult(HttpStatusCode statusCode)
+        {
+            this.StatusCode = statusCode;
+        }
+
+        internal HttpResult(HttpStatusCode statusCode, string message, string? source = null)
+            : base(message, source)
+        {
+            this.StatusCode = statusCode;
+        }
+
+        public HttpStatusCode StatusCode { get; }
+    }
+}
