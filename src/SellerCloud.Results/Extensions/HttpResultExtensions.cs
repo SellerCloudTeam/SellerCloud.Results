eb8ad817ae1a1522494162ea15250840711c0ffd
Stan <stanimir@sellercloud.com>
2020-03-21 20:20:15 +0200

    Result extensions

---
+++ src/SellerCloud.Results/Extensions/HttpResultExtensions.cs
@@ -0,0 +1,38 @@
+﻿using SellerCloud.Results.Http;
+using System.Threading.Tasks;
+
+namespace SellerCloud.Results
+{
+    public static class HttpResultExtensions
+    {
+        public static async Task<T> ResolveOrThrow<T>(this Task<HttpResult<T>> resultTask)
+            where T : class
+        {
+            HttpResult<T> result = await resultTask;
+
+            return result.ResolveOrThrow();
+        }
+
+        public static T ResolveOrThrow<T>(this HttpResult<T> result)
+            where T : class
+        {
+            result.ThrowIfUnsuccessful();
+
+            return result.Data;
+        }
+
+        public static async Task<Result> GetSuccessOrErrorMessageAsync<T>(this Task<HttpResult<T>> resultTask)
+            where T : class
+        {
+            HttpResult<T> result = await resultTask;
+
+            return result.GetSuccessOrErrorMessage();
+        }
+
+        public static Result GetSuccessOrErrorMessage<T>(this HttpResult<T> result)
+            where T : class
+            => result.IsSuccessful
+            ? ResultFactory.Success()
+            : ResultFactory.Error(result.Message);
+    }
+}
---
+++ src/SellerCloud.Results/Extensions/HttpValueResultExtensions.cs
@@ -0,0 +1,38 @@
+﻿using SellerCloud.Results.Http;
+using System.Threading.Tasks;
+
+namespace SellerCloud.Results
+{
+    public static class HttpValueResultExtensions
+    {
+        public static async Task<T> ResolveOrThrow<T>(this Task<HttpValueResult<T>> resultTask)
+            where T : struct
+        {
+            HttpValueResult<T> result = await resultTask;
+
+            return result.ResolveOrThrow();
+        }
+
+        public static T ResolveOrThrow<T>(this HttpValueResult<T> result)
+            where T : struct
+        {
+            result.ThrowIfUnsuccessful();
+
+            return result.Data;
+        }
+
+        public static async Task<Result> GetSuccessOrErrorMessageAsync<T>(this Task<HttpValueResult<T>> resultTask)
+            where T : struct
+        {
+            HttpValueResult<T> result = await resultTask;
+
+            return result.GetSuccessOrErrorMessage();
+        }
+
+        public static Result GetSuccessOrErrorMessage<T>(this HttpValueResult<T> result)
+            where T : struct
+            => result.IsSuccessful
+            ? ResultFactory.Success()
+            : ResultFactory.Error(result.Message);
+    }
+}
